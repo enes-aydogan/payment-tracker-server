01699b51f7f945d8f5e39044098cd2e45ad38a24
enes-aydogan <ensaydgn66@gmail.com>
2021-10-08 21:30:04 +0300

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require('dotenv').config({path: './config/.env'});
-const express = require('express');
+require("dotenv").config({ path: "./config/.env" });
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Enes Aydoğan arkadaşım 2');
-})
+app.get("/", (req, res) => {
+  res.send("Enes Aydoğan arkadaşım 1");
+});
 
 app.listen(3000, () => {
-    console.log('BAŞARILI!');
-})
+  console.log("BAŞARILI!");
+});
