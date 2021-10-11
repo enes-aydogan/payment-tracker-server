6ff6cdfbb99a0ffc643d3f7de763e694107cd4ed
Bülent Demir <bulent7demir@gmail.com>
2021-10-12 00:33:29 +0300

    cors fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors())
+app.use(cors("*"))
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
