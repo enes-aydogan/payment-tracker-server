afa7eb23e1a3b8c41fe630475d5beb8410413717
Bülent Demir <bulent7demir@gmail.com>
2021-10-12 00:43:22 +0300

    limit up

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ require('dotenv').config({path: './config/.env'});
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser')
 
 const db = require('./utils/db');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
 app.use(cors("*"))
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-
 
 const usrRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
