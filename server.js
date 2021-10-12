5b95df7fec85a9eb221028efa3cecc8938a655d8
Bülent Demir <bulent7demir@gmail.com>
2021-10-12 19:12:42 +0300

    fix

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const db = require('./utils/db');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
 app.use(cors("*"))
+app.use('/uploads', express.static('uploads'));
 
 const usrRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
