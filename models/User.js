d61915707829e18909e0dddfe3ee4a5148e335a8
enes-aydogan <ensaydgn66@gmail.com>
2023-02-13 15:32:24 +0300

    .fix

--- models/User.js
+++ models/User.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    firstName:  {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    }
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("User", schema);
